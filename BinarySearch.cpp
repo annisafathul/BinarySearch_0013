ace3ea6b46ec12a537c98ab281acd18beb4c2bf3
annisafathul <annisafathul18@gmail.com>
2026-04-16 15:53:42 +0700

    hitung panjang

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -59,3 +59,14 @@ void display()
     cout << "====================================\n";
     cout << " Elemen Array Setelah Diurutkan (Asc)\n";
     cout << "====================================\n";
+
+    for (int j = 0; j < nPanjang; j++) 
+    {
+        cout << element[j];
+        if (j < nPanjang - 1) 
+        {
+            cout << " -> ";
+        }
+    }
+    cout << endl;
+}
